856206ff5f55412be3c6c2fcd909410fb5e0c025
Josuã Krick <josuakrick@gmail.com>
2022-04-14 11:23:46 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector ('.header nav')
+const toggle = document.querySelectorAll ('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle ('show')
+    })
+}
+
+document.body.onload = function () {
+    if ( document.body.clientWidth >= 1180) {
+       nav.classList.add('show')
+    }
+    else {
+        nav.classList.remove('show')
+    }
+}
